31d712c14da0fa728a31df330695490b6c5c5675
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-24 19:01:51 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,36 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int minSteps, totalSteps;
+ private int totalDays, actDays;
+
+ public StepTracker(int goal)
+ {
+  minSteps = goal;
+  totalSteps = 0;
+  totalDays = 0;
+  actDays = 0;
+ }
+
+ public voidDailySteps(int steps)
+ {
+  totalSteps += stepsl
+   totalDays++;
+  if (steps >= minSteps){
+   actDays++;
+ }
+ }
+
+ public int activeDays(){
+  return actDays;
+ }
+
+ public double averageSteps(){
+ if(totalDays==0)
+  return 0.0;
+  else
+  return (double)totalSteps/totalDays;
+   
+ }
+
 } 
